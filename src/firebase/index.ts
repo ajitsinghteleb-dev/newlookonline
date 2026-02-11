5c365467d1738c503419d4d04b7c446d98d4453c
AJIT singh <ajitsinghteleb@gmail.com>
2026-02-11 19:02:50 +0000

    Try fixing this error: `Runtime FirebaseError: Firebase: Error (auth/fir

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -4,6 +4,7 @@ import { firebaseConfig } from '@/firebase/config';
 import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore'
+import { initializeAppCheck, ReCaptchaV3Provider } from 'firebase/app-check';
 
 // IMPORTANT: DO NOT MODIFY THIS FUNCTION
 export function initializeFirebase() {
@@ -24,6 +25,18 @@ export function initializeFirebase() {
       }
       firebaseApp = initializeApp(firebaseConfig);
     }
+
+    if (typeof window !== 'undefined') {
+      if (process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY && process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY !== 'REPLACE_WITH_YOUR_RECAPTCHA_V3_SITE_KEY') {
+        initializeAppCheck(firebaseApp, {
+          provider: new ReCaptchaV3Provider(process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY),
+          isTokenAutoRefreshEnabled: true,
+        });
+      } else {
+        console.warn('Firebase App Check is not initialized. Make sure to set NEXT_PUBLIC_RECAPTCHA_SITE_KEY in your environment variables.');
+      }
+    }
+
 
     return getSdks(firebaseApp);
   }
